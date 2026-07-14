4b3edc360e184bfc2614d8260feff1dd134bf65e
wanguo <guohaoprc@163.com>
2026-07-14 11:43:22 +0800

    feat: update AM62x raw boot configuration

--- board/ti/am62x/local.mk
+++ board/ti/am62x/local.mk
@@ -3,11 +3,11 @@
 # Keep product-specific source-tree selections in the external tree instead of
 # relying on an untracked local.mk in the Buildroot root directory.
 
-UBOOT_OVERRIDE_SRCDIR = $(TOPDIR)/../uboot/ti-u-boot-2025.10
+UBOOT_OVERRIDE_SRCDIR = $(TOPDIR)/../uboot/u-boot-2026.04
 LINUX_OVERRIDE_SRCDIR = $(TOPDIR)/../linux/ti-linux-kernel-6.18.13
 
 # PAF local source used when BR2_PACKAGE_PAF is enabled.
 PAF_OVERRIDE_SRCDIR = $(TOPDIR)/../paf
 
 # TI K3 R5 Loader uses the same U-Boot source tree as the A53 U-Boot
-TI_K3_R5_LOADER_OVERRIDE_SRCDIR = $(TOPDIR)/../uboot/ti-u-boot-2025.10
+TI_K3_R5_LOADER_OVERRIDE_SRCDIR = $(TOPDIR)/../uboot/u-boot-2026.04
